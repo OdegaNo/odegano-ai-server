fdde78601e7ac387a4e7f9f68afe510f1dd4b6ab
kmg <legolove08@naver.com>
2025-11-16 22:35:19 +0900

    feat: llm_client

---
+++ src/llm/llm_client.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+import os
+
+load_dotenv()
+
+_llm = None
+
+def get_llm() -> ChatOpenAI:
+    global _llm
+    if _llm is None:
+        _llm = ChatOpenAI(
+            model="gpt-4o-mini",
+            temperature=0,
+            api_key=os.getenv("OPENAI_KEY"),
+        )
+    return _llm
